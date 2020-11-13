306a3ece64d1ba22d94b7ccb4eba35be68505464
F0rkster <matthias-gabel@hotmail.de>
2020-11-13 11:40:40 +0100

    Ready to push

---
+++ hello_world.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Hallo World</title>
+
+</head>
+<body>
+	<?php
+		echo "hello world!";
+	?>
+	<hr>
+	<?="hello world"?>
+</body>
+
+</html>
